db096ccafa9d2ce0ffe41f23161abe095b729b51
Christopher Woggon <christopher.woggon@gmail.com>
2020-09-27 15:30:31 +0200

    Created runtime evaluation test

--- bitflow-controller/pkg/scheduler/evaluation_test.go
+++ bitflow-controller/pkg/scheduler/evaluation_test.go
@@ -1,9 +1,12 @@
 package scheduler
 
 import (
+	"fmt"
 	"github.com/bitflow-stream/bitflow-k8s-operator/bitflow-controller/pkg/common"
 	"github.com/stretchr/testify/suite"
+	"strconv"
 	"testing"
+	"time"
 )
 
 type EvaluationTestSuite struct {
@@ -14,7 +17,7 @@ func TestEvaluation(t *testing.T) {
 	suite.Run(t, new(EvaluationTestSuite))
 }
 
-func (s *EvaluationTestSuite) Test_AdvancedScheduler_should() {
+func (s *EvaluationTestSuite) Test_AdvancedScheduler_shouldComparePenaltiesForSimpleCase() {
 	curve := Curve{
 		a: 6.71881241016441,
 		b: 0.0486498280492762,
@@ -134,4 +137,85 @@ func (s *EvaluationTestSuite) Test_AdvancedScheduler_should() {
 
 	println(perfectPenalty)
 	println(actualPenalty)
+}
+
+func (s *EvaluationTestSuite) Test_AdvancedScheduler_shouldPrint3DGraphData() {
+	maxNumberOfNodes := 50
+	maxNumberOfPods := 50
+
+	type Measurement struct {
+		numberOfNodes int
+		numberOfPods  int
+		executionTime time.Duration
+	}
+
+	var measurements []Measurement
+
+	curve := Curve{
+		a: 6.71881241016441,
+		b: 0.0486498280492762,
+		c: 2.0417306475862214,
+		d: 15.899403720950454,
+	}
+
+	var scheduler AdvancedScheduler
+	scheduler = AdvancedScheduler{
+		networkPenalty:                 200,
+		memoryPenalty:                  1_000_000,
+		executionTimePenaltyMultiplier: 2,
+		thresholdPercent:               5,
+		nodes:                          []*NodeData{},
+		pods:                           []*PodData{},
+	}
+
+	for numberOfNodes := 1; numberOfNodes <= maxNumberOfNodes; numberOfNodes++ {
+		scheduler.pods = []*PodData{}
+		scheduler.nodes = append(scheduler.nodes,
+			&NodeData{
+				name:                    "n" + strconv.Itoa(numberOfNodes),
+				allocatableCpu:          4000,
+				memory:                  320,
+				initialNumberOfPodSlots: 2,
+				podSlotScalingFactor:    2,
+				resourceLimit:           0.1,
+			})
+		for numberOfPods := 1; numberOfPods <= maxNumberOfPods; numberOfPods++ {
+			scheduler.pods = append(scheduler.pods,
+				&PodData{
+					name:                 "p" + strconv.Itoa(numberOfPods),
+					dataSourceNodes:      []string{},
+					sendsDataTo:          []string{},
+					curve:                curve,
+					minimumMemory:        16,
+					maximumExecutionTime: 200,
+				})
+
+			println(fmt.Sprintf("measuring %d nodes x %d pods", numberOfNodes, numberOfPods))
+			start := time.Now()
+			scheduler.Schedule()
+			elapsed := time.Since(start)
+
+			measurements = append(measurements, Measurement{
+				numberOfNodes: numberOfNodes,
+				numberOfPods:  numberOfPods,
+				executionTime: elapsed,
+			})
+		}
+	}
+
+	for i := 1; i <= maxNumberOfPods; i++ {
+		print(";")
+		print(i)
+	}
+	println()
+	for _, measurement := range measurements {
+		if measurement.numberOfPods == 1 {
+			print(measurement.numberOfNodes)
+			print(";")
+		}
+		print(fmt.Sprintf("%d;", measurement.executionTime.Nanoseconds()))
+		if measurement.numberOfPods == maxNumberOfPods {
+			println()
+		}
+	}
 }
